54eec510fb845ff45733ed3be457e37aef66ea51
Elson Costa <elsongabriel@hotmail.com>
2016-06-16 17:48:27 -0300

    Atualizando o diretório.

--- app/src/main/java/egcservice/com/studioatendimentos/MainActivity.java
+++ app/src/main/java/egcservice/com/studioatendimentos/MainActivity.java
@@ -1,7 +1,7 @@
-package egcservice.com.br.studioatendimentos;
+package egcservice.com.studioatendimentos;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
